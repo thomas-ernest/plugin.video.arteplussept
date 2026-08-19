916bb92f686af56740d60429ecec476da97dcd89
thomas-ernest <thomas.ernest@gmail.com>
2026-08-19 23:41:54 +0200

    Remove mention of xbmcswift2 in dependencies and documentation. Fix filename for playlist api log

--- plugin.video.arteplussept/addon.py
+++ plugin.video.arteplussept/addon.py
@@ -1,27 +1,4 @@
 """Kodi add-on plugin.video.arteplussept entry point delegates to plugin"""
-# coding=utf-8
-# -*- coding: utf-8 -*-
-#
-# plugin.video.arteplussept, Kodi add-on to watch videos from http://www.arte.tv/guide/fr/plus7/
-# Copyright (C) 2015  known-as-bmf
-# Copyright (C) 2023  thomas-ernest
-#
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-#
-# https://xbmcswift2.readthedocs.io/en/latest/api.html
-# https://github.com/XBMC-Addons/script.module.xbmcswift2
 
 from resources.lib.plugin import plugin
 
